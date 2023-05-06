d37563b5876a940114a66b81d808ba7d0c594918
itchenfei <itchenfei@gmail.com>
2023-05-06 22:49:22 +0800

    update skeleton

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+Author: github: @itchenfei
+"""
 from urllib.request import Request, urlopen
 import logging
 import json
@@ -24,7 +27,8 @@ class CloudflareDDNS:
         logger.info("Config file path: %s", config_file_full_path)
         if os.path.exists(config_file_full_path) is False:
             logger.info("Config file not found in %s", config_file_full_path)
-            config_file_full_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), config_file)
+            script_path = os.path.dirname(os.path.realpath(__file__))
+            config_file_full_path = os.path.join(script_path, config_file)
             logger.info("Try new config file path: %s", config_file_full_path)
 
         # Read config
@@ -53,24 +57,29 @@ class CloudflareDDNS:
         sys.exit(1)
 
     def get_domain_ipv6_address(self, timeout=5):
-        logger.info(f"Get IPv6 address for {self.host_name}")
+        """
+        Get domain IPv6 address use socket, try 10 times if fail.
+        """
+        logger.info("Get IPv6 address for %s", self.host_name)
         for i in range(1, 11):
             with closing(socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)) as sock:
                 sock.settimeout(timeout)
                 try:
                     addr_info = socket.getaddrinfo(self.host_name, None, socket.AF_INET6)
                     ipv6_address = addr_info[0][4][0]
-                    logger.info(f"IPv6 address for {self.host_name} is {ipv6_address}")
+                    logger.info("IPv6 address for %s is %s", self.host_name, ipv6_address)
                     return ipv6_address
                 except (socket.gaierror, socket.timeout):
-                    logger.error(f"Fail to get IPv6 address for {self.host_name}, retry {i}")
+                    logger.error("Fail to get IPv6 address for %s, retry %s", self.host_name, i)
                     time.sleep(1)
-        else:
-            logger.error(f"Fail to get IPv6 address for {self.host_name}")
-            sys.exit(1)
+        logger.error("Fail to get IPv6 address for %s", self.host_name)
+        sys.exit(1)
 
     @staticmethod
     def build_request(url, headers, method='GET', data=None):
+        """
+        Build request object for urlopen
+        """
         req = Request(
             url,
             headers=headers,
@@ -80,6 +89,9 @@ class CloudflareDDNS:
         return req
 
     def make_request(self, url, headers, method='GET', data=None):
+        """
+        Make request to cloudflare API using urllib. Return json data.
+        """
         if isinstance(data, dict):
             data = json.dumps(data).encode()
         elif data is not None:
@@ -89,9 +101,12 @@ class CloudflareDDNS:
         return json.loads(content)
 
     def get_dns_id_and_ip(self):
+        """
+        Get DNS ID and IP from cloudflare API.
+        """
         # Build API
         list_dns_api = f"{self.url}/{self.zone_id}/dns_records?type={self.dns_type}&name={self.host_name}"
-        logger.info(f"List DNS API: {list_dns_api}")
+        logger.info("List DNS API: %s", list_dns_api)
 
         # Build request
         auth_headers = {
@@ -105,13 +120,16 @@ class CloudflareDDNS:
         result = json_data['result'][0]
         dns_id = result.get('id')
         dns_content = result.get('content')
-        logger.info(f"DNS ID: {dns_id}, DNS IP: {dns_content}")
+        logger.info("DNS ID: %s, DNS IP: %s", dns_id, dns_content)
         if not dns_id:
             logger.error("Fail to get DNS ID")
             sys.exit(1)
         return dns_id, dns_content
 
     def update_dns(self, current_ip, dns_id):
+        """
+        Update DNS record using cloudflare API if IP is changed.
+        """
         update_dns_api = f"{self.url}/{self.zone_id}/dns_records/{dns_id}"
         auth_headers = {
             'Authorization': self.api_key,
@@ -123,20 +141,23 @@ class CloudflareDDNS:
             'content': current_ip,
             'proxy': 'false'
         }
-        logger.info(f"Update DNS API: {update_dns_api}")
-        logger.info(f"Update DNS data: {data}")
+        logger.info("Update DNS API: %s", update_dns_api)
+        logger.info("Update DNS data: %s", data)
         json_data = self.make_request(update_dns_api, auth_headers, method='PUT', data=data)
 
         result = json_data['result']
-        logger.info("result: " + str(result))
+        logger.info("result: %s", result)
         dns_name = result.get('name')
         dns_ip = result.get('content')
         if current_ip != dns_ip:
             logger.error("Fail to update DNS")
             sys.exit(1)
-        logger.info(f"dns updated:{dns_name}: {dns_ip}")
+        logger.info("dns updated: %s: %s", dns_name, dns_ip)
 
     def run(self):
+        """
+        All in one
+        """
         # Get current ip and domain ip
         current_ip = self.get_latest_ipv6_addr()
 
@@ -152,6 +173,6 @@ class CloudflareDDNS:
 
 
 if __name__ == "__main__":
-    _config_file = "config.ini"
-    cf_ddns = CloudflareDDNS(_config_file)
+    CONFIG_FILE = "config.ini"
+    cf_ddns = CloudflareDDNS(CONFIG_FILE)
     cf_ddns.run()
